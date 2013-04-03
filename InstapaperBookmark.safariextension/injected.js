77991b3cb3c15ff76ee83415ed7682f05bef96b4
Chen Yufei <cyf@chenyufei.info>
2013-04-03 11:25:21 +0800

    Avoid sending url in iframe.

--- InstapaperBookmark.safariextension/injected.js
+++ InstapaperBookmark.safariextension/injected.js
@@ -1,7 +1,10 @@
-function handleMessage(event) {
-	if (event.name === 'eval') {
-		// alert(event.message);
-		eval(event.message);
-	}
+// Avoid iframes to execute script.
+if (window.top === window) {
+	handleMessage = function (event) {
+		if (event.name === 'eval') {
+			// alert(event.message);
+			eval(event.message);
+		}
+	};
+	safari.self.addEventListener("message", handleMessage, false);
 }
-safari.self.addEventListener("message", handleMessage, false);
